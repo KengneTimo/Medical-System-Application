e30b41e9b277439e402ed7cbbfc63d616c5fe40d
PrinceMUGABE <princemugabe568@gmail.com>
2023-12-29 09:17:50 +0200

    first commit

--- src/main/java/com/medicalApp/serviceImplementation/IllnessNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.medicalApp.serviceImplementation;
-
-public class IllnessNotFoundException extends Throwable {
-    public IllnessNotFoundException(String s) {
-    }
-}
--- src/main/java/com/medicalApp/serviceImplementation/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.medicalApp.serviceImplementation;
-
-public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
